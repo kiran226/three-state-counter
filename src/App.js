520ba3d2cd1666537a6a76fd2cf8a6be9b4d559c
kiran <gandikotakiran99@gmail.com>
2022-01-11 14:43:40 +0530

    counter application using hooks

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React,{useState} from 'react';
+import './App.css'
+
+function App(){
+  const [count,setCount] = useState(0);
+  return(
+    <div className='App'>
+      <header>
+        <h1>Counter App using state/hooks</h1>
+      </header>
+
+      <h2>Current value of state is {count}</h2>
+      <button onClick={() => setCount(0) }>Reset the count</button>
+      <button onClick={() => setCount(count+1) }>Increase the count</button>
+      <button onClick={() => setCount(count-1)} >Decrease the count</button>
+    </div>
+  )
+}
+export default App;
